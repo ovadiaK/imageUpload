af3db71f865c9fbe6c6c6139ae856a45358a3c85
ok <ykelenzon@gmail.com>
2020-09-15 20:08:52 +0300

    fix: required manual img-folder creation

--- server.go
+++ server.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/ovadiaK/imageUpload/img"
 	"github.com/ovadiaK/imageUpload/router"
+	"os"
 )
 
 func main() {
+	initFolders()
 	fmt.Println("running on :8080")
 	router.Router()
+}
+func initFolders() {
+	if _, err := os.Stat(img.IMAGE_FOLDER_TEMP); err != nil {
+		if err := os.Mkdir(img.IMAGE_FOLDER_TEMP, 0777); err != nil {
+			panic(fmt.Errorf("create temporary image folder at %s failed: %s", img.IMAGE_FOLDER_TEMP, err))
+		}
+	}
+	if _, err := os.Stat(img.IMAGE_FOLDER_PERM); err != nil {
+		if err := os.Mkdir(img.IMAGE_FOLDER_PERM, 0777); err != nil {
+			panic(fmt.Errorf("create permanent image folder at %s failed: %s", img.IMAGE_FOLDER_PERM, err))
+		}
+	}
 }
